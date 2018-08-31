d1cdb70499717d0d0b3dc4b3fd858fc2d9f794b6
Baron_Nash <745352049@qq.com>
2018-08-31 14:01:32 +0800

    Initial Commit

---
+++ PageViewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PageViewController
+//
+//  Created by 水晶岛 on 2018/8/31.
+//  Copyright © 2018年 水晶岛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PageViewController/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  PageViewController
+//
+//  Created by 水晶岛 on 2018/8/31.
+//  Copyright © 2018年 水晶岛. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
